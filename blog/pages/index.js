753f5d25332244d0d37b1f8f200197bc44600ac8
NaomiKleinhans <naomikleinhans56@gmail.com>
2024-05-10 23:19:01 +0200

    progress

---
+++ blog/pages/index.js
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import Head from 'next/head'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Page() {
+	return (
+		<>
+			<Head>
+				<title>Create Next App</title>
+			</Head>
+			<div>blog</div>
+		</>
+	)
+}
